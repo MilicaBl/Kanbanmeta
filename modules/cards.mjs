7afe4de62a6a53ad587bdf9ba073b67a436b6bcf
villiam damstrom <villiam@live.se>
2024-10-09 17:21:22 +0200

    Update card.mjs
    Updated the card.mjs with function to create the card columns and a btn "lägg till ett kort" for each column and a function for the main board with the four main coulmns.

--- modules/cards.mjs
+++ modules/cards.mjs
@@ -1,1 +1,44 @@
-console.log("test")
+// Function to create a card column
+export function createCardColumn(title) {
+    const column = document.createElement('div');
+    column.classList.add('card-column');
+
+    // Column title
+    const columnTitle = document.createElement('h2');
+    columnTitle.textContent = title;
+    column.appendChild(columnTitle);
+
+    // Column body
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    column.appendChild(cardBody);
+
+    // Add card button
+    const addCardBtn = document.createElement('button');
+    addCardBtn.classList.add('add-card-btn');
+    addCardBtn.textContent = "Lägg till ett kort";
+    column.appendChild(addCardBtn);
+
+
+}
+
+return coulmn;
+
+// Function to create main board with all columns
+export function createMainBoard() {
+    const root = document.getElementById('root');
+
+    // Create main container
+    const mainContainer = document.createElement('div');
+    mainContainer.classList.add('main-container');
+
+    // Create each card folumn
+    const columns = ['To do', 'Doing', 'Testing', 'Done'];
+    columns.forEach(title => {
+        const column = createCardColumn(title);
+
+        mainContainer.appendChild(column);
+    });
+
+    root.appendChild(mainContainer);
+}
